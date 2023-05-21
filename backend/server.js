23a290893c896a2ecddc5d0b9e4b85427df44fc1
Monique Chan <moniqueczy@hotmail.com>
2023-05-21 09:28:51 +0800

    add backed

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+//TopSocial git init
+//.gitignore(node_modules/) backend
+
+//cd backend
+//npm init  , create server.js
+//npm i axios cors express json-server
+//npm i nodemon concurrently -D
+
+//check git remote -v
